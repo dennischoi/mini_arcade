163753ac1e5e0935829bdc52e7bff496eb11c3ce
Dennis Choi <dennis.choi7@gmail.com>
2016-07-18 19:51:54 -0400

    - Basic build and variables for the snake game input into the JS files and html linked to css and js

---
+++ assets/js/snakes.js
@@ -0,0 +1,45 @@
+// CONSTANTS
+
+var COLS = 26, ROWS = 26;
+
+// IDs
+
+var EMPTY = 0, SNAKE = 1, FRUIT = 2;
+
+
+var grid = {
+      width: null,
+      height: null,
+      _grid: null,
+
+      init: function(d, c, r) {
+
+      },
+
+      set: function(val, x, y) {
+
+      },
+
+      get: function(x, y) {
+
+      }
+}
+
+
+var snake = {
+
+      direction: null,
+      _queue: null,
+
+      init: function(d, x, y) {
+
+      },
+
+      insert: function (x, y) {
+
+      },
+
+      remove: function() {
+        
+      }
+}
